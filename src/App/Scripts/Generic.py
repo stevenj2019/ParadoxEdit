63bfd8a38c5945c76c704ec4a5587ee4ac2ca1bf
stevenj2019 <steven.j2019@pm.me>
2026-06-30 23:38:21 +0100

    forgot to delete some old code, lol

--- src/App/Scripts/Generic.py
+++ src/App/Scripts/Generic.py
@@ -2,16 +2,6 @@ from ParadoxParser import ParadoxScriptParser as PDXScriptFile
 from ParadoxParser.ParadoxNodes import GenericBlock, GenericComment
 
 from App.Enums import ChangeState
-#OLD AND BAD
-# def clear_comments(file, app_controller):
-#     def remove_comments(node):
-#         if isinstance(node, GenericBlock):
-#             node.children = [c for c in node.children if not isinstance(c, GenericComment)]
-#     script = file.obj
-#     script.nodes = [node for node in script.nodes if not isinstance(node, GenericComment)]
-#     for root in script.nodes:
-#         if isinstance(root, GenericBlock):
-#             root.traverse(remove_comments)
 
 def clear_comments(file, app_controller):
     def tombstone_comments(node):
