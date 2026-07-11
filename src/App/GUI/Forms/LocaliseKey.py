89eb7f14d8bdc871d3524eeca35680c9b5c928c4
stevenj2019 <steven.j2019@pm.me>
2026-07-11 16:37:41 +0100

    cleanup in Localisation Form(s)

--- src/App/GUI/Forms/LocaliseKey.py
+++ src/App/GUI/Forms/LocaliseKey.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import QDialog, QFormLayout, QLabel, QLineEdit, QTextEdit, QComboBox, QPushButton
 
-from ParadoxParser.ParadoxNodes import GenericLocKey
+from ParadoxParser.ParadoxNodes import GenericBlock, GenericLocKey
 
 # from App.Modules.Loc import LocalisationCategory
 from App.Contracts import NodeMutationRequest, BlockMutationRequest
@@ -14,7 +14,6 @@ class BaseLocaliseForm(QDialog):
         self.category = app_controller.registry.get_category(CATEGORY)
         self.category_meta = app_controller.registry.get_category_metadata(CATEGORY)
         
-        self.resize(550, 150)#this will likely need an edit
         self.setLayout(QFormLayout())
         self.form = self.layout()
 
@@ -102,87 +101,6 @@ class LocaliseNodeForm(BaseLocaliseForm):
             self.app_controller.request_block_mutation.emit(
                 BlockMutationRequest.add(self.save_file, len(self.save_file.nodes)+1, node, self.save_file)
             )
-
-# class LocaliseNodeForm(QDialog):
-#     def __init__(self, app_controller, key:str=None):
-#         super().__init__()
-        # self.app_controller = app_controller
-        # self.category = app_controller.registry.get_category(CATEGORY)
-        # self.category_meta = app_controller.registry.get_category_metadata(CATEGORY)
-        # self.loc_key = key
-        # if self.loc_key and self.loc_key in self.category_meta.keys():
-        #     self.file_selected = self.category_meta[self.loc_key]["file"]
-        #     self.node_selected = self.category_meta[self.loc_key]["node"]
-        # else:
-        #     self.file_selected = None
-        #     self.node_selected = None
-
-        # self.setWindowTitle("Localise Key")
-        # self.resize(550, 150)
-        # self.setLayout(QFormLayout())
-        # self.form = self.layout()
-
-        # self.localisation_label = QLabel(self.loc_key)
-        # self.localisation_text = QTextEdit()
-        # if self.node_selected is not None:
-        #     self.localisation_text.setPlainText(self.node_selected.value)
-        # self._handle_localisation_field()
-        # self.localisation_label.setBuddy(self.localisation_text)
-        # self.localisation_text.textChanged.connect(self._resize_localisation_field)
-        # self.form.addRow(self.localisation_label, self.localisation_text)
-
-    #     self.save_to_file_label = QLabel("Localisation File:")
-    #     self.file_dropdown = QComboBox()
-    #     for index, _file in enumerate(self.category.files.values()):
-    #         self.file_dropdown.addItem(_file.filename)
-    #         if _file is self.file_selected:
-    #             self.file_dropdown.setCurrentIndex(index)
-    #             self.file_dropdown.setEnabled(False)
-    #             self.file_selected = self.category.files[self.file_dropdown.itemText(index)]
-    #     if not self.file_selected:
-    #         text = self.file_dropdown.currentText()
-    #         self.file_selected = self.category.files[text]
-
-    #     self.save_to_file_label.setBuddy(self.file_dropdown)
-    #     self.file_dropdown.currentIndexChanged.connect(self._change_save_file)
-    #     self.form.addRow(self.save_to_file_label, self.file_dropdown)
-
-    #     self.submit_button = QPushButton("Continue")
-    #     self.form.addRow(self.submit_button)
-    #     self.submit_button.clicked.connect(self._submit)
-    #     self.exec_()
-
-    # def _change_save_file(self, index):
-    #     file = self.file_dropdown.itemText(index)
-    #     self.file_selected = self.category.files[file]
-    
-    # def _handle_localisation_field(self):
-    #     text = self.localisation_text.toPlainText()
-    #     text = text.replace("\\n", "\n")
-
-    #     if text != self.localisation_text.toPlainText():
-    #         self.localisation_text.blockSignals(True)
-    #         self.localisation_text.setPlainText(text)
-    #         self.localisation_text.blockSignals(False)
-
-    #     self._resize_localisation_field()
-    
-    # def _resize_localisation_field(self):
-    #     doc_height = self.localisation_text.document().size().height()
-    #     new_height = max(30, int(doc_height+1))
-    #     new_height = min(new_height, 250)
-
-    #     self.localisation_text.setFixedHeight(new_height)
-
-    # def _submit(self):
-    #     self._handle_localisation_field()
-    #     new_value = self.localisation_text.toPlainText().replace("\n", "\\n")
-    #     if self.node_selected is not None:
-    #         self.app_controller.request_node_mutation.emit(
-    #             NodeMutationRequest(file=self.file_selected, node=self.node_selected, node_value=self.node_selected, value=new_value)
-    #         )
-    #     else:
-    #         node = GenericLocKey(self.loc_key, new_value)
-    #         self.app_controller.request_block_mutation.emit(
-    #             BlockMutationRequest.add(self.file_selected, len(self.file_selected.nodes)+1, node, self.file_selected)
-    #         )
+# class LocaliseEventForm(BaseLocaliseForm):
+#     def __init__(self, app_controller, block:GenericBlock):
+#         super().__init__(app_controller, "Localise Event")
