4858a76f13e4a4855d5ed607e014ea6a773919ec
stevenj2019 <steven.j2019@pm.me>
2026-07-27 23:23:17 +0100

    fixed loc metadata collection

--- src/App/Loading/Directories/Loc.py
+++ src/App/Loading/Directories/Loc.py
@@ -4,7 +4,7 @@ if TYPE_CHECKING:
     from App.Loading.ParadoxSource import ParadoxSource
 
 import os
-
+from pathlib import Path
 from ParadoxParser import ParadoxLocParser as PDXLocFile
 from ParadoxParser.ParadoxNodes import GenericLegacyLocKey, GenericLocKey
 
@@ -19,22 +19,23 @@ class LocDirectory(GenericDirectory):
     def __init__(self, source:ParadoxSource,file_path:os.PathLike, read_only:bool):
         super().__init__(source, file_path, FILE_TYPES, PDXLocFile, read_only)
     
-    # def metadata_collection(self, source, file):
-    #     metadata = dict()
-    #     metadata[PDXMetadata.LanguageKey] = set()
-    #     metadata[PDXMetadata.LocKey] = dict()
-    #     for directory in self.directories.values():
-    #         language_key = directory.path.parts[-1]
-    #         metadata[PDXMetadata.LanguageKey].add(language_key)
-    #         for file in directory.files.values():
-    #             file = file.file
-    #             for node in file.nodes:
-    #                 if isinstance(node, (GenericLocKey, GenericLegacyLocKey)):
-    #                     metadata[PDXMetadata.LocKey].setdefault(node.key, dict())
-    #                     metadata[PDXMetadata.LocKey][node.key][language_key] = {
-    #                         "file":file, "node":node
-    #                     }
-    #     return metadata
+    def metadata_collection(self, source, file):
+        metadata = dict()
+        metadata[PDXMetadata.LanguageKey] = set()
+        metadata[PDXMetadata.LocKey] = dict()
+        file = file.file
+        if "_l_" not in file.filename:
+            return metadata
+        language = Path(file.filename).stem.rsplit("_l_", 1)[1]
+        language_key = f"l_{language}"
+        metadata[PDXMetadata.LanguageKey].add(language_key)
+        for node in file.nodes:
+            if isinstance(node, (GenericLocKey, GenericLegacyLocKey)):
+                metadata[PDXMetadata.LocKey].setdefault(node.key, dict())
+                metadata[PDXMetadata.LocKey][node.key][language_key] = {
+                    "file":file, "node":node
+                }
+        return metadata
     
     def resolve_context(self, file):
         if file.endswith("gfx"):
